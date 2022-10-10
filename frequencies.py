40d24804162512cc646f52ea88ea031b45e77218
Ismail Doghri <ismail.doghri@kcl.ac.uk>
2022-10-10 17:52:08 +0100

    Final solution

--- frequencies.py
+++ frequencies.py
@@ -4,4 +4,10 @@
 def frequencies(items):
     frequencies = {}
     # Your code goes here
+    strItems = []
+    for item in items:
+        strItems.append(str(item))
+
+    frequencies = {item: strItems.count(item) for item in strItems}
+
     return frequencies
